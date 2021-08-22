6e11a7639a53ff5e9d6e1581646a6837aad104d7
Willem Jacobs <wamj58@gmail.com>
2021-08-21 22:12:28 -0500

    lab13

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Carousel from "react-bootstrap/Carousel";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
 import { withAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 
@@ -12,30 +15,84 @@ class MyFavoriteBooks extends React.Component {
     super(props);
     this.state = {
       bestBooks: [],
+      showModal: false,
     };
   }
 
   componentDidMount = async () => {
-    const { getIdTokenClaims } = this.props.auth0;
+    const { getIdTokenClaims, user } = this.props.auth0;
     let tokenClaims = await getIdTokenClaims();
     const jwt = tokenClaims.__raw;
     const config = {
       headers: { Authorization: `Bearer ${jwt}` },
     };
+    try {
+      const results = await axios.get(
+        `http://localhost:3001/books?email=${user.email}`,
+        config
+      );
+      this.setState({
+        bestBooks: results.data,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    const results = await axios.get("http://localhost:3001/books", config);
-    console.log(results.data);
-    this.setState({
-      bestBooks: results.data,
-    });
+  closeModalHandler = () => {
+    this.setState({ showModal: false });
+  };
+
+  showModalHandler = () => {
+    this.setState({ showModal: true });
+  };
+
+  addNewBookSubmitHandler = (event) => {
+    event.preventDefault();
+    const bookTitle = event.target.bookTitle.value;
+    const bookDescription = event.target.bookDescription.value;
+    const bookStatus = event.target.bookStatus.value;
+    const newBookObject = {
+      title: bookTitle,
+      description: bookDescription,
+      status: bookStatus,
+      email: this.props.auth0.user.email,
+    };
+    this.closeModalHandler();
+    // console.log(newBookObject);
+    this.postNewBookHandler(newBookObject);
+  };
+
+  postNewBookHandler = async (newBookObject) => {
+    try {
+      const results = await axios.post(
+        "http://localhost:3001/books",
+        newBookObject
+      );
+      this.setState({ bestBooks: [...this.state.bestBooks, results.data] });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  deleteBookHandler = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3001/books/${id}`);
+      let remainingBooks = this.state.bestBooks.filter(
+        (book) => book._id !== id
+      );
+      this.setState({ bestBooks: remainingBooks });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   render() {
-    let renderBooks = <h1>No Books found. Try addng some</h1>;
+    let renderBooks = <h1>No Books found. Try adding some</h1>;
     if (this.state.bestBooks.length > 0) {
       renderBooks = this.state.bestBooks.map((book, index) => {
         return (
-          <Carousel.Item key={index}>
+          <Carousel.Item key={book._id}>
             <Card style={{ width: "20rem", height: "20rem" }}>
               <Card.Body>
                 <Card.Title>
@@ -43,6 +100,10 @@ class MyFavoriteBooks extends React.Component {
                 </Card.Title>
                 <Card.Text>
                   <h4>{book.description}</h4>
+                  <p>{book.status}</p>
+                  <Button onClick={() => this.deleteBookHandler(book._id)}>
+                    Delete Book
+                  </Button>
                   <p>Email: {book.email}</p>
                 </Card.Text>
               </Card.Body>
@@ -52,7 +113,46 @@ class MyFavoriteBooks extends React.Component {
       });
     }
 
-    return <Carousel>{renderBooks}</Carousel>;
+    return (
+      <>
+        <Modal show={this.state.showModal} onHide={this.closeModalHandler}>
+          <Modal.Header>
+            <Modal.Title>Add New Book</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.addNewBookSubmitHandler}>
+              <Form.Group controlId="bookTitle">
+                <Form.Label>Book Title</Form.Label>
+                <Form.Control type="text" placeholder="Enter Title" />
+              </Form.Group>
+              <Form.Group controlId="bookDescription">
+                <Form.Label>Book Description</Form.Label>
+                <Form.Control type="text" placeholder="Enter Description" />
+              </Form.Group>
+              <Form.Group controlId="bookStatus">
+                <Form.Label>Book Status</Form.Label>
+                <Form.Control as="select">
+                  <option value="LIFE-CHANGING">Life Changing</option>
+                  <option value="FAVORITE FIVE">Favorite Five</option>
+                  <option value="RECOMENDED TO ME">Recomended to Me</option>
+                </Form.Control>
+              </Form.Group>
+              <Button type="submit">Save Book</Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.closeModalHandler}>
+              Close
+            </Button>
+            {/* <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button> */}
+          </Modal.Footer>
+        </Modal>
+        <Carousel>{renderBooks}</Carousel>
+        <Button onClick={this.showModalHandler}>Add Book</Button>
+      </>
+    );
   }
 }
 
